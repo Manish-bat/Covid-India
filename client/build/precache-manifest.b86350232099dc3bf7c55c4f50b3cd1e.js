7ac222efc729c014a3e5899c3fa803fafc471cec
Manish-bat <manishyadave800@gmail.com>
2021-01-01 17:36:42 +0530

    add static

---
+++ client/build/precache-manifest.b86350232099dc3bf7c55c4f50b3cd1e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c4842bbc8dd0f88e279cb42c3e0301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f7bfce5263fa3bed39",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "399d0f0c12efd69d6b7f",
+    "url": "/static/css/main.5299e57b.chunk.css"
+  },
+  {
+    "revision": "63f7bfce5263fa3bed39",
+    "url": "/static/js/2.ef466c5b.chunk.js"
+  },
+  {
+    "revision": "a2e3d86a5813ce7e4677a58842078442",
+    "url": "/static/js/2.ef466c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399d0f0c12efd69d6b7f",
+    "url": "/static/js/main.0f4e96bd.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
